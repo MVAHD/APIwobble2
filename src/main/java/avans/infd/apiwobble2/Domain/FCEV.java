139a47a5dac3dda46ee5c555d354f9abe8a10ae8
MVAHD <marijn.van.aalst1@gmail.com>
2021-11-04 16:08:03 +0100

    types toegevoegd

---
+++ src/main/java/avans/infd/apiwobble2/Domain/FCEV.java
@@ -0,0 +1,12 @@
+package avans.infd.apiwobble2.Domain;
+
+//Makes type of object of Car
+public class FCEV extends Car{
+    //Declaration
+    private final String type;
+
+    //Initialisation
+    {
+        type = "Fuel Cell Electric Vehicle";
+    }
+}
