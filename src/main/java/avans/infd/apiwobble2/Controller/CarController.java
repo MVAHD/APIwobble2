ef870fe283b5c803b9e487e9f490851dc6b49d8f
MVAHD <marijn.van.aalst1@gmail.com>
2021-11-06 13:52:22 +0100

    update voor auke

--- src/main/java/avans/infd/apiwobble2/Controller/CarController.java
+++ src/main/java/avans/infd/apiwobble2/Controller/CarController.java
@@ -1,40 +1,30 @@
 package avans.infd.apiwobble2.Controller;
 
-import avans.infd.apiwobble2.Domain.BEV;
 import avans.infd.apiwobble2.Domain.Car;
-import avans.infd.apiwobble2.Domain.FCEV;
-import avans.infd.apiwobble2.Domain.ICE;
 import avans.infd.apiwobble2.Service.CarService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.converter.json.GsonBuilderUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+//Car controller layer of the APIwobble.
 @RestController
 @RequestMapping(path = "api/car")
 public class CarController {
-//    private CarService service;
-//
-//    @Autowired
-//    public CarController(CarService service) {
-//        this.service = service;
-//    }
+    private final CarService service;
 
-    Car car1 = new ICE("05KSN7", "Metalic");
-    Car car2 = new FCEV("RS809V", "Perlmore");
-    Car car3 = new ICE("9XXK14", "Blauw");
-    Car car4 = new BEV("DS059T", "Black");
+    //Connect automatically to the Car service layer.
+    @Autowired
+    public CarController(CarService service) {
+        this.service = service;
+    }
 
-    //Instantiate pool
-
-    //Add cars to pool
-
+    //Get the list of cars of the selected pool from the service.
     @GetMapping(path = "pool")
     public List<Car> getPoolCars(){
-        //Todo: make for pool
-        return List.of(car1, car2, car3, car4);
+        return service.getPoolCars();
     }
+
 }
--- src/main/java/avans/infd/apiwobble2/Service/CarService.java
+++ src/main/java/avans/infd/apiwobble2/Service/CarService.java
@@ -1,4 +1,27 @@
 package avans.infd.apiwobble2.Service;
 
+import avans.infd.apiwobble2.Domain.BEV;
+import avans.infd.apiwobble2.Domain.Car;
+import avans.infd.apiwobble2.Domain.FCEV;
+import avans.infd.apiwobble2.Domain.ICE;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CarService {
+
+    Car car1 = new ICE("05KSN7", "Metalic");
+    Car car2 = new FCEV("RS809V", "Perlmore");
+    Car car3 = new ICE("9XXK14", "Blauw");
+    Car car4 = new BEV("DS059T", "Black");
+
+    //Instantiate pool
+
+    //Add cars to pool
+
+    public List<Car> getPoolCars(){
+        //Todo: make for pool
+        return List.of(car1, car2, car3, car4);
+    }
 }
