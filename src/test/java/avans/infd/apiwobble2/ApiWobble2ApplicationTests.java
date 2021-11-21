750f79ff97814cb4ece435e058655017ba9084c3
Auk-A <auke.aardoom@gmail.com>
2021-11-21 20:01:26 +0100

    Tests: added datetimeformatter

--- src/test/java/avans/infd/apiwobble2/ApiWobble2ApplicationTests.java
+++ src/test/java/avans/infd/apiwobble2/ApiWobble2ApplicationTests.java
@@ -2,13 +2,16 @@ package avans.infd.apiwobble2;
 
 import avans.infd.apiwobble2.Domain.User;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
+
 class ApiWobble2ApplicationTests {
+
+    DateTimeFormatter df = DateTimeFormatter.ofPattern("dd-MM-yyyy");
 
     @Test
     void contextLoads() {
@@ -16,22 +19,20 @@ class ApiWobble2ApplicationTests {
 
     @Test
     public void instantiateUserGetUserAge_expectNotNull() {
-        User user = new User("test", LocalDate.parse("01-01-1990"));
+        User user = new User("test", LocalDate.parse("01-01-1990", df));
         assertNotNull(user.getAge());
     }
 
     @Test
     public void instantiateTwoUsersWithSameBirthDate_expectTrue() {
-        User user1 = new User("test1", LocalDate.parse("01-01-1995"));
-        User user2 = new User("test1", LocalDate.parse("01-01-1995"));
+        User user1 = new User("test1", LocalDate.parse("01-01-1995", df));
+        User user2 = new User("test2", LocalDate.parse("01-01-1995", df));
         assertEquals(user1.getAge(), user2.getAge());
     }
 
-
     @Test
     public void testPassword() {
-        // - todo - add test for constraint violation
-        User user = new User("", LocalDate.parse("01-01-1990"));
+        User user = new User("", LocalDate.parse("01-01-1990", df));
         user.setPassword("thisIsTest");
     }
 
